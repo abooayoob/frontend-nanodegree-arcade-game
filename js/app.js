98d4bba0c6e5cd4db9de03dac74c4d7e46ff8ea1
Mohammad Ali Khan <brorentilhammad@gmail.com>
2016-04-06 14:23:57 +0200

    Stubs out some required functions, and makes the game render.

--- js/app.js
+++ js/app.js
@@ -25,12 +25,42 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+  
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var numberOfEnemies = 3, allEnemies = [], newEnemy, player;
 
-
+for (var i = 0; i < numberOfEnemies; i++) {
+  newEnemy = new Enemy();
+  allEnemies.push(newEnemy);
+}
+player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
