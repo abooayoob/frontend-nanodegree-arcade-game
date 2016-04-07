ef407e277d17529da93b170038d00aeda64f2bd1
Mohammad Ali Khan <brorentilhammad@gmail.com>
2016-04-07 15:34:37 +0200

    Makes 30 bugs and moves them across the screen

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var start = 0;
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -6,6 +14,11 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = -200;
+    this.y = getRandomInt(1, 3)*73;
+    this.startTime = getRandomInt(1, 30)*80;
+    this.wait = 0;
+    this.speed = getRandomInt(200, 500);
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +27,14 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // Check if this enemy should move, if not update the wait parameter
+    if (this.wait>this.startTime && this.x<700) {
+      this.x  += this.speed*dt;
+    }else {
+      ++this.wait;
+    }
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -48,13 +69,13 @@ Player.prototype.render = function() {
 };
 
 Player.prototype.handleInput = function() {
-  
+
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var numberOfEnemies = 3, allEnemies = [], newEnemy, player;
+var numberOfEnemies = 30, allEnemies = [], newEnemy, player;
 
 for (var i = 0; i < numberOfEnemies; i++) {
   newEnemy = new Enemy();
